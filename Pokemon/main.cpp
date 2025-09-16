f04986ea162567e4f5eeed2fe564367522ecbf6f
Raghuvansh Agarwal <rarchva@gmail.com>
2025-09-16 15:16:47 +0530

    Your First Pokemon

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,22 +2,31 @@
 
 int main() {
 
-    std::cout << "Hello brave adventurer, it's me Professor Oak, what is your name? ";
+    std::cout << "[Professor Oak] Ah, Trainer!" << std::endl;
+    std::cout << "[Professor Oak] Welcome to the world of Pokémon!" << std::endl;
+    std::cout << "[Professor Oak] What should the world call you? ";
     std::string player_name;
     std::cin >> player_name;
-    std::cout << player_name << ", I think you are ready for your first Pokemon, so which pokemon will it be?" << std::endl;
+    std::cout << "[" << player_name << "] My name is " << player_name << std::endl;
+
+    std::cout << "[Professor Oak] Today is a momentous day—you’ll be choosing your very first Pokémon." << std::endl;
+    std::cout << "[Professor Oak] Every great Trainer remembers this moment for the rest of their lives." << std::endl;
+    std::cout << "[Professor Oak] So, choose wisely, young one!" << std::endl;
     std::cout << "1) Bulbasaur\n2) Charmander\n3) Squirtle" << std::endl;
     std::cout << "Enter the number" << std::endl;
     int choice;
     std::cin >> choice;
     if (choice == 1) {
-        std::cout << "Excellent choice by going with Bulbasaur" << std::endl;
+        std::cout << "[" << player_name << "] I want Bulbasaur" << std::endl;
+        std::cout << "[Professor Oak] Excellent choice by going with Bulbasaur" << std::endl;
     }
     else if (choice == 2) {
-        std::cout << "Charmander, A fiery choice" << std::endl;
+        std::cout << "[" << player_name << "] I want Charmander" << std::endl;
+        std::cout << "[Professor Oak] Charmander, A fiery choice" << std::endl;
     }
     else if (choice == 3) {
-        std::cout << "Squirtle is a wonderful Pokemon to start" << std::endl;
+        std::cout << "[" << player_name << "] I want Squirtle" << std::endl;
+        std::cout << "[Professor Oak] Squirtle is a wonderful Pokemon to start" << std::endl;
     }
     else {
         std::cout << "Invalid choice, restart the Game" << std::endl;
