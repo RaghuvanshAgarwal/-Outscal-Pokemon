755f388516017c63e9d34b6767884443252579b0
Raghuvansh Agarwal <rarchva@gmail.com>
2025-09-22 13:28:41 +0530

    Cleaning my main file

---
+++ Pokemon/Game/Game.h
@@ -0,0 +1,17 @@
+//
+// Created by Raghuvansh Agarwal on 22/09/25.
+//
+
+#ifndef POKEMON_GAME_H
+#define POKEMON_GAME_H
+
+
+class Player;
+
+class Game {
+public:
+    void gameLoop(Player& player);
+};
+
+
+#endif //POKEMON_GAME_H
